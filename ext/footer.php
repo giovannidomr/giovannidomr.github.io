bab39b32c311223058606b96e9769d9393eeb845
giovannidomr <121050309+giovannidomr@users.noreply.github.com>
2022-12-19 11:11:54 -0600

    Add files via upload

---
+++ ext/footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+	<div class="container-md">
+		<hr class="sep-footer"></hr>
+			<div class="row">
+					<div class="col-6 text-sec-footer" align="left">
+						Todos los derechos reservados. TuColor™ 2022
+					</div class="col-6">
+					<div class="col-6 text-sec-footer" align="right">
+						Desarrollado por <b>INNOTECR®</b>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
+
+<script src="js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://use.fontawesome.com/releases/v6.2.0/js/all.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script>
+		AOS.init();
+	</script>
